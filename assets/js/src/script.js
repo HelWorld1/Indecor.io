b8b8a1496a403a5220dcdee05480d6929817bcde
Dmitry Grigorev <helworldd@yandex.ru>
2022-03-26 14:29:27 +0300

    Update

--- assets/js/src/script.js
+++ assets/js/src/script.js
@@ -1,2 +1,7 @@
-alert("hellworld");
-console.log('test');
+$(document).ready(function(){
+  $('.burger').click(function(){
+      $('.nav-panel__li').toggleClass('mobile_hide');
+      $('.nav-panel__social').toggleClass('mobile_hide');
+      $('main,footer').toggleClass('_opacity');
+  })
+});

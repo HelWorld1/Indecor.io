28bca4be6a089121b86b9631c94e1f98948f095a
Dmitry Grigorev <helworldd@yandex.ru>
2022-04-04 19:27:54 +0300

    Update

--- assets/js/src/script.js
+++ assets/js/src/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+  if($(window).width() <= '576'){
+    $('.information__list').toggleClass('desktop');
+  };
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = $(this.hash)
+    $('html, body').stop().animate({
+      'scrollTop': target.offset().top
+    }, 1000);
+  });
   $('.burger').click(function(){
-      $('.nav-panel__li').toggleClass('mobile_hide');
-      $('.nav-panel__social').toggleClass('mobile_hide');
-      $('main,footer').toggleClass('_opacity');
-  })
+    $('.nav-panel__li').toggleClass('mobile_hide');
+    $('.nav-panel__social').toggleClass('mobile_hide');
+    $('main,footer').toggleClass('_opacity');
+  });
+  $('.information__list').click(function(){
+    $(this).toggleClass('noactive');
+    $(this).toggleClass('active');
+  });
 });
